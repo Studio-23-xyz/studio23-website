1a65f28cbfa71d6fbdb214794fc4c3d03ed70313
Shahadat Hossain Shamim <shamim131666@gmail.com>
2024-04-26 17:28:46 +0600

    Hero Banner Complete

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -5,95 +5,95 @@ import { Facebook, Instagram, Linkedin, Twitter } from "lucide-react";
 
 const Footer = () => {
   return (
-    <div className="w-full md:h-[300px] bg-footer_black flex md:px-32 md:pt-10">
+    <div className="w-full md:h-[376px] bg-footer_black flex md:pt-10">
       {/* Laptop View */}
 
-      <div className="hidden md:flex">
-        <div className="w-1/4 flex flex-col gap-14">
-          <div>
-            <Link href="/">
-              <Image
-                src="/assets/studio23_logo_white.png"
-                width={500}
-                height={200}
-                alt="Logo"
-                className="w-[150px] md:w-[165px] pl-2 md:pl-0 pt-2 md:pt-0"
-              />
-            </Link>
-          </div>
-          <div>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry.
-            </p>
-          </div>
-          <div>
-            <p>@Lorem</p>
-          </div>
-        </div>
-        <div className="w-1/4 flex flex-col items-center justify-center">
-          <div className="flex flex-col gap-4">
+      <div className="hidden md:flex flex-col">
+        <div className="flex md:px-32">
+          <div className="w-1/4 flex flex-col gap-10">
             <div>
-              <Link href="/about-us">
-                <span className="font-semibold">About Us</span>
+              <Link href="/">
+                <Image
+                  src="/assets/studio23_logo_white.png"
+                  width={500}
+                  height={200}
+                  alt="Logo"
+                  className="w-[150px] md:w-[165px] pl-2 md:pl-0 pt-2 md:pt-0"
+                />
               </Link>
             </div>
-            <div className="flex flex-col gap-2">
-              <Link href="/our-team">
-                <span className="">Our Team</span>
-              </Link>
-              <Link href="/our-titles">
-                <span className="">Titles</span>
-              </Link>
-              <Link href="/careers">
-                <span className="">Careers</span>
-              </Link>
-              <Link href="/contact-us">
-                <span className="">Contact Us</span>
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div className="w-1/4 flex flex-col items-center justify-center">
-          <div className="flex flex-col gap-5">
             <div>
-              <p className="font-semibold text-[17px]">Contact Us</p>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry.
+              </p>
             </div>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry.
-            </p>
-            <h3>+88 01200000000</h3>
+            <div>
+              <p>@Lorem</p>
+            </div>
+          </div>
+          <div className="w-1/4 flex flex-col items-center justify-center">
+            <div className="flex flex-col gap-4">
+              <div>
+                <Link href="/about-us">
+                  <span className="font-semibold">About Us</span>
+                </Link>
+              </div>
+              <div className="flex flex-col gap-2">
+                <Link href="/our-team">
+                  <span className="">Our Team</span>
+                </Link>
+                <Link href="/our-titles">
+                  <span className="">Titles</span>
+                </Link>
+                <Link href="/careers">
+                  <span className="">Careers</span>
+                </Link>
+                <Link href="/contact-us">
+                  <span className="">Contact Us</span>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="w-1/4 flex flex-col items-center justify-center">
+            <div className="flex flex-col gap-5">
+              <div>
+                <p className="font-semibold text-[17px]">Contact Us</p>
+              </div>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry.
+              </p>
+              <h3>+88 01200000000</h3>
+            </div>
+          </div>
+          <div className="w-1/4 flex flex-row gap-4 justify-end items-end pb-12">
+            <div className="bg-white  w-[35px] h-[35px] rounded-full flex justify-center items-center hover:bg-studio_blue">
+              <a href="https://www.facebook.com/Studio23.BS23">
+                <Facebook className="stroke-black w-[20px] h-[20px] hover:stroke-white" />
+              </a>
+            </div>
+            <div className="bg-white  w-[35px] h-[35px] rounded-full flex justify-center items-center hover:bg-studio_blue">
+              <a href="https://www.instagram.com/studio23xyz/?hl=en">
+                <Instagram className="stroke-black w-[20px] h-[20px] hover:stroke-white" />
+              </a>
+            </div>
+            <div className="bg-white  w-[35px] h-[35px] rounded-full flex justify-center items-center hover:bg-studio_blue">
+              <a href="https://twitter.com/Studio23xyz">
+                <Twitter className="stroke-black w-[20px] h-[20px] hover:stroke-white" />
+              </a>
+            </div>
+            <div className="bg-white  w-[35px] h-[35px] rounded-full flex justify-center items-center hover:bg-studio_blue">
+              <a href="https://www.linkedin.com/company/studio-23-bs/">
+                <Linkedin className="stroke-black w-[20px] h-[20px] hover:stroke-white" />
+              </a>
+            </div>
           </div>
         </div>
-        <div className="w-1/4 flex flex-row gap-4 justify-end items-end pb-12">
-          <div className="bg-white  w-[35px] h-[35px] rounded-full flex justify-center items-center hover:bg-studio_blue">
-            <a href="https://www.facebook.com/Studio23.BS23">
-              <Facebook className="stroke-black w-[20px] h-[20px] hover:stroke-white" />
-            </a>
-          </div>
-          <div className="bg-white  w-[35px] h-[35px] rounded-full flex justify-center items-center hover:bg-studio_blue">
-            <a href="https://www.instagram.com/studio23xyz/?hl=en">
-              <Instagram className="stroke-black w-[20px] h-[20px] hover:stroke-white" />
-            </a>
-          </div>
-          <div className="bg-white  w-[35px] h-[35px] rounded-full flex justify-center items-center hover:bg-studio_blue">
-            <a href="https://twitter.com/Studio23xyz">
-              <Twitter className="stroke-black w-[20px] h-[20px] hover:stroke-white" />
-            </a>
-          </div>
-          <div className="bg-white  w-[35px] h-[35px] rounded-full flex justify-center items-center hover:bg-studio_blue">
-            <a href="https://www.linkedin.com/company/studio-23-bs/">
-              <Linkedin className="stroke-black w-[20px] h-[20px] hover:stroke-white" />
-            </a>
-          </div>
-        </div>
-        <div className="border-t-2">
-          <div className="mt-4 flex justify-center">
-            <p className=" text-[14px]">
-              Copyright ® Studio23 2024 All rights Rcerved
-            </p>
-          </div>
+        <div className="border-t-2 mt-8 flex justify-center">
+          <p className="text-[14px] mt-8">
+            Copyright ® Studio23 2024 All rights Rcerved
+          </p>
         </div>
       </div>
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,30 @@
+"use client";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <div className="absulate -z-10 w-full">
+      {/* Hero Banner */}
+      <div className=" -z-10 w-full">
+        {/* Background Image */}
         <Image
-          src="/assets/hero-homepage.jpg"
-          width={1409}
-          height={842}
-          alt="hero"
-          className="w-full md:h-screen bg-no-repeat"
+          src="/assets/SilentScreamBGLarge.png"
+          width={1439}
+          height={837}
+          alt="Hero BG"
+          className="w-full h-[303px] md:h-screen"
         />
+        {/* Skull  */}
+        <div className="absolute top-[119px] md:top-[-40px] left-[173px] md:left-[675px]">
+          <Image
+            src="/assets/silentScreamSkull.png"
+            width={765}
+            height={871}
+            alt="Silent Scream Skull"
+            className="w-[208px] md:w-[880px] h-[221px] md:h-[935px]"
+          />
+        </div>
+        {/* Logo & others */}
         <div className="flex flex-col justify-start w-[200px] md:w-[750px] absolute gap-2 md:gap-5 top-14 md:top-48 pl-6 md:pl-32">
           <Image
             src="/assets/silentscream_logo.png"
@@ -23,18 +37,29 @@ export default function Home() {
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard.
           </p>
-          <button className="text-start w-[264px] h-[50px] bg-studio_blue font-bold text-[16px] text-black flex justify-center items-center gap-2 rounded-3xl">
+          <button
+            className="text-start w-[132px] md:w-[264px] h-[25px] md:h-[50px] bg-studio_blue font-bold text-[8px] md:text-[16px] text-black 
+          flex justify-center items-center gap-2 rounded-3xl hover:bg-white hover:cursor-pointer"
+            onClick={() => {
+              window.open(
+                "https://store.steampowered.com/app/1955750/SILENT_SCREAM/",
+                "_blank"
+              );
+            }}
+          >
             Check out on Steam
             <Image
               src="/assets/steam-icon.png"
               width={30}
               height={30}
               alt="silent-scream-logo"
-              className="w-[30px] h-[30px]"
+              className="w-[15px] md:w-[30px] h-[15px] md:h-[30px]"
             />
           </button>
         </div>
       </div>
+
+      {/* Body Section*/}
       <div>
         <div className="flex">
           <div>
