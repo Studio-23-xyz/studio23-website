280386fc2ea4ca7c826c90feec6219f213f77a9f
Fayaze H. Pantho <fayazexo@gmail.com>
2024-05-19 00:12:50 +0600

    👌 IMPROVE: Change direct imports to relative imports for images

--- src/app/components/common/Footer.js
+++ src/app/components/common/Footer.js
@@ -4,215 +4,215 @@ import Link from "next/link";
 import { Facebook, Instagram, Linkedin, Twitter } from "lucide-react";
 
 const Footer = () => {
-  return (
-    <div className="w-full md:h-[376px] bg-footer_black flex md:flex-col md:pt-10">
-      {/* Laptop View */}
-      <div className="hidden md:flex flex-col">
-        <div className="flex md:px-32">
-          <div className="w-1/4 flex flex-col gap-10">
-            <div>
-              <Link href="/">
-                <Image
-                  src="/assets/studio23_logo_white.png"
-                  width={500}
-                  height={200}
-                  alt="Logo"
-                  className="w-[150px] md:w-[165px] pl-2 md:pl-0 pt-2 md:pt-0"
-                />
-              </Link>
-            </div>
-            <div>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry.
-              </p>
-            </div>
-            <div>
-              <p>@Lorem</p>
-            </div>
-          </div>
-          <div className="w-1/4 flex flex-col items-center justify-center">
-            <div className="flex flex-col gap-4">
-              <div>
-                <Link href="/about-us">
-                  <span className="font-semibold">About Us</span>
-                </Link>
-              </div>
-              <div className="flex flex-col gap-2">
-                <Link href="/our-team">
-                  <span className="">Our Team</span>
-                </Link>
-                <Link href="/our-titles">
-                  <span className="">Titles</span>
-                </Link>
-                <Link href="/careers">
-                  <span className="">Careers</span>
-                </Link>
-                <Link href="/contact-us">
-                  <span className="">Contact Us</span>
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div className="w-1/4 flex flex-col items-center justify-center">
-            <div className="flex flex-col gap-5">
-              <div>
-                <p className="font-semibold text-[17px]">Contact Us</p>
-              </div>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry.
-              </p>
-              <h3>+88 01200000000</h3>
-            </div>
-          </div>
-          <div className="w-1/4 flex flex-row gap-4 justify-end items-end pb-12">
-            <div className="bg-white  w-[35px] h-[35px] rounded-full flex justify-center items-center hover:bg-studio_blue">
-              <a
-                href="https://www.facebook.com/Studio23.BS23"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Facebook className="stroke-black w-[20px] h-[20px] hover:stroke-white" />
-              </a>
-            </div>
-            <div className="bg-white  w-[35px] h-[35px] rounded-full flex justify-center items-center hover:bg-studio_blue">
-              <a
-                href="https://www.instagram.com/studio23xyz/?hl=en"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Instagram className="stroke-black w-[20px] h-[20px] hover:stroke-white" />
-              </a>
-            </div>
-            <div className="bg-white  w-[35px] h-[35px] rounded-full flex justify-center items-center hover:bg-studio_blue">
-              <a
-                href="https://twitter.com/Studio23xyz"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Twitter className="stroke-black w-[20px] h-[20px] hover:stroke-white" />
-              </a>
-            </div>
-            <div className="bg-white  w-[35px] h-[35px] rounded-full flex justify-center items-center hover:bg-studio_blue">
-              <a
-                href="https://www.linkedin.com/company/studio-23-bs/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Linkedin className="stroke-black w-[20px] h-[20px] hover:stroke-white" />
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="hidden md:flex border-t-2 mt-8  justify-center">
-        <p className="text-[14px] mt-8">
-          Copyright ® Studio23 2024 All rights Rcerved
-        </p>
-      </div>
+	return (
+		<div className="w-full md:h-[376px] bg-footer_black flex md:flex-col md:pt-10">
+			{/* Laptop View */}
+			<div className="hidden md:flex flex-col">
+				<div className="flex md:px-32">
+					<div className="w-1/4 flex flex-col gap-10">
+						<div>
+							<Link href="/">
+								<Image
+									src="./assets/studio23_logo_white.png"
+									width={500}
+									height={200}
+									alt="Logo"
+									className="w-[150px] md:w-[165px] pl-2 md:pl-0 pt-2 md:pt-0"
+								/>
+							</Link>
+						</div>
+						<div>
+							<p>
+								Lorem Ipsum is simply dummy text of the printing and typesetting
+								industry.
+							</p>
+						</div>
+						<div>
+							<p>@Lorem</p>
+						</div>
+					</div>
+					<div className="w-1/4 flex flex-col items-center justify-center">
+						<div className="flex flex-col gap-4">
+							<div>
+								<Link href="/about-us">
+									<span className="font-semibold">About Us</span>
+								</Link>
+							</div>
+							<div className="flex flex-col gap-2">
+								<Link href="/our-team">
+									<span className="">Our Team</span>
+								</Link>
+								<Link href="/our-titles">
+									<span className="">Titles</span>
+								</Link>
+								<Link href="/careers">
+									<span className="">Careers</span>
+								</Link>
+								<Link href="/contact-us">
+									<span className="">Contact Us</span>
+								</Link>
+							</div>
+						</div>
+					</div>
+					<div className="w-1/4 flex flex-col items-center justify-center">
+						<div className="flex flex-col gap-5">
+							<div>
+								<p className="font-semibold text-[17px]">Contact Us</p>
+							</div>
+							<p>
+								Lorem Ipsum is simply dummy text of the printing and typesetting
+								industry.
+							</p>
+							<h3>+88 01200000000</h3>
+						</div>
+					</div>
+					<div className="w-1/4 flex flex-row gap-4 justify-end items-end pb-12">
+						<div className="bg-white  w-[35px] h-[35px] rounded-full flex justify-center items-center hover:bg-studio_blue">
+							<a
+								href="https://www.facebook.com/Studio23.BS23"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<Facebook className="stroke-black w-[20px] h-[20px] hover:stroke-white" />
+							</a>
+						</div>
+						<div className="bg-white  w-[35px] h-[35px] rounded-full flex justify-center items-center hover:bg-studio_blue">
+							<a
+								href="https://www.instagram.com/studio23xyz/?hl=en"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<Instagram className="stroke-black w-[20px] h-[20px] hover:stroke-white" />
+							</a>
+						</div>
+						<div className="bg-white  w-[35px] h-[35px] rounded-full flex justify-center items-center hover:bg-studio_blue">
+							<a
+								href="https://twitter.com/Studio23xyz"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<Twitter className="stroke-black w-[20px] h-[20px] hover:stroke-white" />
+							</a>
+						</div>
+						<div className="bg-white  w-[35px] h-[35px] rounded-full flex justify-center items-center hover:bg-studio_blue">
+							<a
+								href="https://www.linkedin.com/company/studio-23-bs/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<Linkedin className="stroke-black w-[20px] h-[20px] hover:stroke-white" />
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="hidden md:flex border-t-2 mt-8  justify-center">
+				<p className="text-[14px] mt-8">
+					Copyright ® Studio23 2024 All rights Rcerved
+				</p>
+			</div>
 
-      {/* Mobile View */}
-      <div className="md:hidden flex flex-col gap-5 mx-4 my-4">
-        <div>
-          <Link href="/">
-            <Image
-              src="/assets/studio23_logo_white.png"
-              width={500}
-              height={200}
-              alt="Logo"
-              className="w-[138px]"
-            />
-          </Link>
-        </div>
-        <div>
-          <p className="text-[16px]">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry.
-          </p>
-        </div>
-        <div className="flex flex-row">
-          <div className="w-1/2 flex flex-col gap-4">
-            <div>
-              <Link href="/about-us">
-                <span className="font-semibold text-[17px]">About Us</span>
-              </Link>
-            </div>
-            <div className="flex flex-col gap-3">
-              <Link href="/our-team">
-                <span className="">Our Team</span>
-              </Link>
-              <Link href="/our-titles">
-                <span className="">Titles</span>
-              </Link>
-              <Link href="/careers">
-                <span className="">Careers</span>
-              </Link>
+			{/* Mobile View */}
+			<div className="md:hidden flex flex-col gap-5 mx-4 my-4">
+				<div>
+					<Link href="/">
+						<Image
+							src="./assets/studio23_logo_white.png"
+							width={500}
+							height={200}
+							alt="Logo"
+							className="w-[138px]"
+						/>
+					</Link>
+				</div>
+				<div>
+					<p className="text-[16px]">
+						Lorem Ipsum is simply dummy text of the printing and typesetting
+						industry.
+					</p>
+				</div>
+				<div className="flex flex-row">
+					<div className="w-1/2 flex flex-col gap-4">
+						<div>
+							<Link href="/about-us">
+								<span className="font-semibold text-[17px]">About Us</span>
+							</Link>
+						</div>
+						<div className="flex flex-col gap-3">
+							<Link href="/our-team">
+								<span className="">Our Team</span>
+							</Link>
+							<Link href="/our-titles">
+								<span className="">Titles</span>
+							</Link>
+							<Link href="/careers">
+								<span className="">Careers</span>
+							</Link>
 
-              <p className="">Contact Us</p>
-            </div>
-          </div>
+							<p className="">Contact Us</p>
+						</div>
+					</div>
 
-          <div className="w-1/2 flex flex-col gap-4">
-            <div>
-              <p className="font-semibold text-[17px]">Contact Us</p>
-            </div>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry.
-            </p>
-            <h3>+88 01200000000</h3>
-          </div>
-        </div>
-        <div className="flex gap-5 mb-3">
-          <div className="bg-white  w-[35px] h-[35px] rounded-full flex justify-center items-center hover:bg-studio_blue">
-            <a
-              href="https://www.facebook.com/Studio23.BS23"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Facebook className="stroke-black w-[20px] h-[20px] hover:stroke-white" />
-            </a>
-          </div>
-          <div className="bg-white  w-[35px] h-[35px] rounded-full flex justify-center items-center hover:bg-studio_blue">
-            <a
-              href="https://www.instagram.com/studio23xyz/?hl=en"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Instagram className="stroke-black w-[20px] h-[20px] hover:stroke-white" />
-            </a>
-          </div>
-          <div className="bg-white  w-[35px] h-[35px] rounded-full flex justify-center items-center hover:bg-studio_blue">
-            <a
-              href="https://twitter.com/Studio23xyz"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Twitter className="stroke-black w-[20px] h-[20px] hover:stroke-white" />
-            </a>
-          </div>
-          <div className="bg-white  w-[35px] h-[35px] rounded-full flex justify-center items-center hover:bg-studio_blue">
-            <a
-              href="https://www.linkedin.com/company/studio-23-bs/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Linkedin className="stroke-black w-[20px] h-[20px] hover:stroke-white" />
-            </a>
-          </div>
-        </div>
+					<div className="w-1/2 flex flex-col gap-4">
+						<div>
+							<p className="font-semibold text-[17px]">Contact Us</p>
+						</div>
+						<p>
+							Lorem Ipsum is simply dummy text of the printing and typesetting
+							industry.
+						</p>
+						<h3>+88 01200000000</h3>
+					</div>
+				</div>
+				<div className="flex gap-5 mb-3">
+					<div className="bg-white  w-[35px] h-[35px] rounded-full flex justify-center items-center hover:bg-studio_blue">
+						<a
+							href="https://www.facebook.com/Studio23.BS23"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<Facebook className="stroke-black w-[20px] h-[20px] hover:stroke-white" />
+						</a>
+					</div>
+					<div className="bg-white  w-[35px] h-[35px] rounded-full flex justify-center items-center hover:bg-studio_blue">
+						<a
+							href="https://www.instagram.com/studio23xyz/?hl=en"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<Instagram className="stroke-black w-[20px] h-[20px] hover:stroke-white" />
+						</a>
+					</div>
+					<div className="bg-white  w-[35px] h-[35px] rounded-full flex justify-center items-center hover:bg-studio_blue">
+						<a
+							href="https://twitter.com/Studio23xyz"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<Twitter className="stroke-black w-[20px] h-[20px] hover:stroke-white" />
+						</a>
+					</div>
+					<div className="bg-white  w-[35px] h-[35px] rounded-full flex justify-center items-center hover:bg-studio_blue">
+						<a
+							href="https://www.linkedin.com/company/studio-23-bs/"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<Linkedin className="stroke-black w-[20px] h-[20px] hover:stroke-white" />
+						</a>
+					</div>
+				</div>
 
-        <div className="border-t-2">
-          <div className="mt-4 flex justify-center">
-            <p className=" text-[13px]">
-              Copyright ® Studio23 2024 All rights Rcerved
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+				<div className="border-t-2">
+					<div className="mt-4 flex justify-center">
+						<p className=" text-[13px]">
+							Copyright ® Studio23 2024 All rights Rcerved
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
--- src/app/components/homepage/RecentEventsSection.js
+++ src/app/components/homepage/RecentEventsSection.js
@@ -2,103 +2,103 @@ import React from "react";
 import Image from "next/image";
 
 const RecentEventsSection = () => {
-  return (
-    <div className="mb-8 md:mb-16">
-      <div className="flex flex-col gap-3 items-center w-full h-[67px] mb-4 md:mb-16">
-        <h1 className="text-[16px] md:text-[30px] font-bold">
-          Our Recent Community Events
-        </h1>
-        <p className="text-[10px] md:text-[14px] font-normal text-center">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry.
-        </p>
-      </div>
-      {/* Grid Section */}
-      <div className="flex flex-col md:flex-row gap-4">
-        <div className="md:w-2/3 flex flex-col gap-4">
-          <div className="flex gap-4">
-            <div className="w-1/2 md:w-3/5">
-              <a
-                href="http://localhost:3000/community"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Image
-                  src="/assets/dev-meetup-24.png"
-                  width={1395}
-                  height={1286}
-                  alt="Dev Meetup"
-                  className="w-[289px] md:w-full h-[139px] md:h-[223px] rounded-2xl object-cover"
-                />
-              </a>
-            </div>
-            <div className="w-1/2 md:w-2/5">
-              <a
-                href="http://localhost:3000/community"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Image
-                  src="/assets/game-jam-home.png"
-                  width={1342}
-                  height={399}
-                  alt="Game Jam Cover"
-                  className="w-[289px] md:w-full h-[139px] md:h-[223px] rounded-2xl object-cover"
-                />
-              </a>
-            </div>
-          </div>
-          <div className="flex gap-4">
-            <div className="w-1/2 md:w-2/5">
-              <a
-                href="https://itch.io/jam/brain-jam/entries"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Image
-                  src="/assets/calculate.png"
-                  width={720}
-                  height={960}
-                  alt="Calculate Theme"
-                  className="w-[289px] md:w-full h-[139px] md:h-[223px] rounded-2xl object-cover"
-                />
-              </a>
-            </div>
-            <div className="w-1/2 md:w-3/5">
-              <a
-                href="http://localhost:3000/community"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Image
-                  src="/assets/hack-jam.jpg"
-                  width={1920}
-                  height={1005}
-                  alt="Hack A Jam"
-                  className="w-[289px] md:w-full h-[139px] md:h-[223px] rounded-2xl object-cover"
-                />
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="md:w-1/3">
-          <a
-            href="https://itch.io/jam/iutcs-hack-a-jam-2021/entries"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/assets/good-bad.jpg"
-              width={1000}
-              height={1000}
-              alt="Bad is Good"
-              className="h-full 2xl:h-[460px] rounded-2xl object-cover"
-            />
-          </a>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="mb-8 md:mb-16">
+			<div className="flex flex-col gap-3 items-center w-full h-[67px] mb-4 md:mb-16">
+				<h1 className="text-[16px] md:text-[30px] font-bold">
+					Our Recent Community Events
+				</h1>
+				<p className="text-[10px] md:text-[14px] font-normal text-center">
+					Lorem Ipsum is simply dummy text of the printing and typesetting
+					industry.
+				</p>
+			</div>
+			{/* Grid Section */}
+			<div className="flex flex-col md:flex-row gap-4">
+				<div className="md:w-2/3 flex flex-col gap-4">
+					<div className="flex gap-4">
+						<div className="w-1/2 md:w-3/5">
+							<a
+								href="http://localhost:3000/community"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<Image
+									src="./assets/dev-meetup-24.png"
+									width={1395}
+									height={1286}
+									alt="Dev Meetup"
+									className="w-[289px] md:w-full h-[139px] md:h-[223px] rounded-2xl object-cover"
+								/>
+							</a>
+						</div>
+						<div className="w-1/2 md:w-2/5">
+							<a
+								href="http://localhost:3000/community"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<Image
+									src="./assets/game-jam-home.png"
+									width={1342}
+									height={399}
+									alt="Game Jam Cover"
+									className="w-[289px] md:w-full h-[139px] md:h-[223px] rounded-2xl object-cover"
+								/>
+							</a>
+						</div>
+					</div>
+					<div className="flex gap-4">
+						<div className="w-1/2 md:w-2/5">
+							<a
+								href="https://itch.io/jam/brain-jam/entries"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<Image
+									src="./assets/calculate.png"
+									width={720}
+									height={960}
+									alt="Calculate Theme"
+									className="w-[289px] md:w-full h-[139px] md:h-[223px] rounded-2xl object-cover"
+								/>
+							</a>
+						</div>
+						<div className="w-1/2 md:w-3/5">
+							<a
+								href="http://localhost:3000/community"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<Image
+									src="./assets/hack-jam.jpg"
+									width={1920}
+									height={1005}
+									alt="Hack A Jam"
+									className="w-[289px] md:w-full h-[139px] md:h-[223px] rounded-2xl object-cover"
+								/>
+							</a>
+						</div>
+					</div>
+				</div>
+				<div className="md:w-1/3">
+					<a
+						href="https://itch.io/jam/iutcs-hack-a-jam-2021/entries"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<Image
+							src="./assets/good-bad.jpg"
+							width={1000}
+							height={1000}
+							alt="Bad is Good"
+							className="h-full 2xl:h-[460px] rounded-2xl object-cover"
+						/>
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default RecentEventsSection;
