a9a5708f9b00a91c7a03d294e4ee6f79420cb43c
Shahadat Hossain Shamim <shamim131666@gmail.com>
2024-05-20 17:25:55 +0600

    Tab System Update

--- src/app/components/silentscream2page/SS2CastComponents.js
+++ src/app/components/silentscream2page/SS2CastComponents.js
@@ -1,40 +1,43 @@
 import { useEffect, useRef, useState } from "react";
 
-const SS2CastComponents = ({items}) => {
+const SS2CastComponents = ({ items }) => {
+  const firstBtnRef = useRef();
+  const [selectedTab, setSelectedTab] = useState(0); // Initialize to the first tab
 
-    const firstBtnRef = useRef();
-    const [selectedTab, setSelectedTab] = useState();
+  useEffect(() => {
+    firstBtnRef.current.focus();
+  }, []);
 
-    useEffect (() => {
-        firstBtnRef.current.focus();
-    }, [])
-
-    return (
-        <div className=" bg-slate-200 flex justify-center items-center py-12">
-            <div className="max-w-md flex flex-col gap-y-2 w-full">
-                <div className=" bg-blue-300 p-1 rounded-xl flex justify-between items-center gap-x-2">
-                    {items.map((item, index) => (
-                        <button
-                        ref={index === 0 ? firstBtnRef : null}
-                        key={index}
-                        onClick={()=> setSelectedTab(index)}
-                         className=" outline-none w-full p-2 hover:bg-slate-500 rounded-xl
+  return (
+    <div className=" bg-slate-200 flex justify-center items-center py-12">
+      <div className="max-w-md flex flex-col gap-y-2 w-full">
+        <div className=" bg-blue-300 p-1 rounded-xl flex justify-between items-center gap-x-2">
+          {items.map((item, index) => (
+            <button
+              ref={index === 0 ? firstBtnRef : null}
+              key={index}
+              onClick={() => setSelectedTab(index)}
+              className=" outline-none w-full p-2 hover:bg-slate-500 rounded-xl
                          focus:ring-2 focus:bg-white focus:text-red-300"
-                        >{item.title}</button>
-                    ))}
-                </div>
-
-                <div>
-                    {items.map((item, index) => (
-                    <div className={`${selectedTab === index ? '' : 'hidden'}`}>
-                        {item.content}
-                    </div>
-                    ))}
-                </div>
-            </div>
-
+            >
+              {item.title}
+            </button>
+          ))}
         </div>
-    );
+
+        <div>
+          {items.map((item, index) => (
+            <div
+              key={index} // Add key prop to avoid warning
+              className={`${selectedTab === index ? "" : "hidden"}`}
+            >
+              {item.content}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SS2CastComponents;
