521d255bbb2a9fd8b126d38d19237ea528c84d14
Shahadat Hossain Shamim <shamim131666@gmail.com>
2024-05-22 19:05:01 +0600

    Titles Page Link Add

--- src/app/our-titles/page.js
+++ src/app/our-titles/page.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Link from "next/link";
 
 const page = () => {
-  return <div className="min-h-screen pt-20">Our Titles Page</div>;
+  return (
+    <div className="min-h-screen pt-20 flex flex-col justify-center items-center gap-6">
+      <div>
+        <Link href="/our-titles/silent-scream-2">
+          <p className="text-[20px] text-silent2_red hover:text-white">
+            Silen Scream 2 Game Page
+          </p>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default page;
