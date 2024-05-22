8e013770a440cd48f1a2aca3f6d712dd00af933a
Shahadat Hossain Shamim <shamim131666@gmail.com>
2024-05-22 15:07:34 +0600

    Test Section Polishing - SS2 Game Page

--- src/app/components/silentscream2page/SS2DirectorSection.js
+++ src/app/components/silentscream2page/SS2DirectorSection.js
@@ -2,47 +2,19 @@ const SS2DirectorSection = () => {
   return (
     <div>
       <div className="w-full flex justify-center items-center mt-6">
-        {/* Laptop View */}
         <div
-          className="md:flex flex-col gap-3 hidden md:w-[1320px] md:h-[432px] bg-ss2-banner-lm bg-full bg-no-repeat 
-          bg-center pt-16"
+          className="flex flex-col justify-center items-center w-[290px] md:w-[1320px] h-[617px] md:h-[432px]
+           bg-ss2-banner-mm md:bg-ss2-banner-lm bg-no-repeat bg-center gap-3 md:gap-9"
         >
           <h3
-            className="text-[40px] font-bold text-silent2_button_text flex justify-center items-center
-             text-center"
-          >
-            WORDS FROM THE GAME DIRECTOR
-          </h3>
-          <p
-            className="flex justify-center items-center text-[20px] font-bold text-silent2_button_text
-              text-center mx-14 leading-10"
-          >
-            Sed lectus consequat convallis faucibus etiam sit mauris hendrerit
-            cursus. Cursus mattis eget placerat et diam. Mauris tortor risus
-            dolor porta aliquam id vestibulum. Magna ipsum erat est augue.
-            Malesuada etiam sit amet sem orci cursus lorem tempus. Et et est et
-            et vitae enim. Nulla dignissim tortor adipiscing cursus egestas nisl
-            egestas. Rhoncus pellentesque enim amet massa pretium massa ut
-            mauris sit. Sit quis volutpat enim dignissim erat eu risus
-            porttitor. Praesent sapien eget velit nulla commodo. Ullamcorper
-            sagittis nunc Ullamcorper sagittis nunc.Ullamcorper sagittis nunc..
-          </p>
-        </div>
-
-        {/* Mobile View */}
-        <div
-          className="flex flex-col gap-2 md:hidden w-[290px] h-[617px] bg-ss2-banner-mm
-                bg-cover bg-center bg-no-repeat pt-14"
-        >
-          <h3
-            className="text-[20px] font-bold text-silent2_button_text mx-8 mt-1
+            className="text-[20px] md:text-[40px] font-bold text-silent2_button_text mx-8 mt-1
              text-center leading-6"
           >
             WORDS FROM THE GAME DIRECTOR
           </h3>
           <p
-            className="flex justify-start items-center text-[14px] font-bold text-silent2_button_text
-               text-center mx-11 leading-5"
+            className="text-[14px] md:text-[20px] font-bold text-silent2_button_text text-center
+           mx-11 md:mx-14 leading-5 md:leading-10 flex justify-center items-center"
           >
             Sed lectus consequat convallis faucibus etiam sit mauris hendrerit
             cursus. Cursus mattis eget placerat et diam. Mauris tortor risus
--- src/app/components/silentscream2page/SS2NoticeSection.js
+++ src/app/components/silentscream2page/SS2NoticeSection.js
@@ -3,38 +3,14 @@ import React from "react";
 const SS2NoticeSection = () => {
   return (
     <div>
-      <div className="w-full flex justify-center items-center">
-        {/* Laptop View */}
+      <div className="w-full flex justify-center items-center mt-10">
         <div
-          className="md:flex hidden md:w-[1320px] md:h-[288px] bg-ss2-banner-ls
-        bg-full bg-no-repeat bg-center"
+          className="bg-ss2-banner-ms md:bg-ss2-banner-ls flex justify-center w-[290px] h-[300px] md:w-[1320px] md:h-[288px] 
+          bg-full bg-no-repeat bg-center"
         >
-          <p
-            className="flex justify-center items-center text-[20px] font-bold text-silent2_button_text
-        text-center mx-14 leading-10"
-          >
-            Silent Scream 2, a spine-chilling third-person adventure puzzle
-            horror game developed by Studio 23, plunges players into the depths
-            of terror within an abandoned asylum, where they must navigate
-            through intricate puzzles, uncover dark mysteries, and confront
-            malevolent entities to survive.
-          </p>
-        </div>
-        {/* Mobile View */}
-        <div
-          className="flex md:hidden w-[290px] h-[300px] bg-ss2-banner-ms
-          bg-cover bg-center bg-no-repeat mt-6"
-        >
-          <p
-            className="flex justify-start items-center text-[14px] font-bold text-silent2_button_text
-         text-center mx-9 leading-5"
-          >
-            Silent Scream 2, a spine-chilling third-person adventure puzzle
-            horror game developed by Studio 23, plunges players into the depths
-            of terror within an abandoned asylum, where they must navigate
-            through intricate puzzles, uncover dark mysteries, and confront
-            malevolent entities to survive.
-          </p>
+          <h1 className="flex items-center text-[50px] font-bold text-silent2_button_text text-center">
+            COMING SOON
+          </h1>
         </div>
       </div>
     </div>
