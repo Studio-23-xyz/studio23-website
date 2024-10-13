2e6cc578b455383d66919a8c6fc53ed86ad7f1f1
Shahadat Hossain Shamim <shamim131666@gmail.com>
2024-10-13 16:40:21 +0600

    Update Job Card

--- src/app/components/careerspage/CareersPageOpeningSection.js
+++ src/app/components/careerspage/CareersPageOpeningSection.js
@@ -4,13 +4,13 @@ import { useRef, useState } from 'react';
 
 
 // JobRow Component: Reusable component for job listings
-const JobRow = ({ title, work, location, category, type, onSeeDetails }) => (
+const JobRow = ({ title, status, workplace, location, deadline, onSeeDetails }) => (
   <tr className="border-b-2 border-studio_blue flex flex-col md:flex-row md:table-row">
     <th className="text-[18px] md:text-[24px] font-bold py-5">{title}</th>
-    <th className="text-[14px] md:text-[16px] font-bold py-1 md:py-5">{work}</th>
+    <th className="text-[14px] md:text-[16px] font-thin py-1 md:py-5">{status}</th>
+    <th className="text-[14px] md:text-[16px] font-thin py-1 md:py-5">{workplace}</th>
     <th className="text-[14px] md:text-[16px] font-thin py-1 md:py-5">{location}</th>
-    <th className="text-[14px] md:text-[16px] font-thin py-1 md:py-5">{category}</th>
-    <th className="text-[14px] md:text-[16px] font-thin py-1 md:py-5">{type}</th>
+    <th className="text-[14px] md:text-[16px] font-thin py-1 md:py-5">{deadline}</th>
     <th className="py-2 md:py-5">
       <button
         className="bg-white text-black text-[14px] md:text-[16px] px-5 py-2 hover:bg-studio_blue transition duration-200 rounded-xl"
@@ -49,11 +49,23 @@ const Modal = ({ isOpen, onClose, jobDetails }) => {
         <X className="text-red-500 hover:text-white" onClick={onClose}/>
         </div>
         
+        <h3 className="text-lg md:text-xl font-semibold mb-2">Vacancy:</h3>
+        <p className="mb-4">{jobDetails.vacancy}</p>
+
+        <h3 className="text-lg md:text-xl font-semibold mb-2">Employment Status:</h3>
+        <p className="mb-4">{jobDetails.status}</p>
+
         <h3 className="text-lg md:text-xl font-semibold mb-2">Job Location:</h3>
         <p className="mb-4">{jobDetails.location}</p>
 
-        <h3 className="text-lg md:text-xl font-semibold mb-2">Salary:</h3>
-        <p className="mb-4">{jobDetails.salary}</p>
+        <h3 className="text-lg md:text-xl font-semibold mb-2">Workplace:</h3>
+        <p className="mb-4">{jobDetails.workplace}</p>
+
+        <h3 className="text-lg md:text-xl font-semibold mb-2">category:</h3>
+        <p className="mb-4">{jobDetails.category}</p>
+
+        <h3 className="text-lg md:text-xl font-semibold mb-2">Education:</h3>
+        <p className="mb-4">{jobDetails.education}</p>
 
         <h3 className="text-lg md:text-xl font-semibold mb-2">Job Description:</h3>
         <p className="mb-4">{jobDetails.description}</p>
@@ -79,6 +91,12 @@ const Modal = ({ isOpen, onClose, jobDetails }) => {
           ))}
         </ul>
 
+        <h3 className="text-lg md:text-xl font-semibold mb-2">Salary:</h3>
+        <p className="mb-4">{jobDetails.salary}</p>
+
+        <h3 className="text-lg md:text-xl font-semibold mb-2">Application Deadline:</h3>
+        <p className="mb-4">{jobDetails.deadline}</p>
+
         <button
           className="bg-studio_blue text-white font-semibold px-4 py-2 rounded hover:bg-white hover:text-black"
           onClick={() => window.open(jobDetails.applyLink, '_blank')}
@@ -97,8 +115,12 @@ const CareersPageOpeningSection = () => {
   const jobOpenings = [
     {
       title: 'Game Developer Trainee',
+      vacancy: '--',
+      status: 'Full Time',
       location: 'Mirpur DOHS, Dhaka, Bangladesh',
-      salary: 'BDT 10,000 / Month',
+      workplace: 'Work at Office',
+      category: 'Gameplay Programmer',
+      education: 'Studio 23 is looking for a passionate and experienced Senior Game Developer to join our growing team.',
       description: 'Studio 23 is looking for a passionate and experienced Senior Game Developer to join our growing team. In this role, you will work closely with designers, artists, and other developers to build innovative and immersive gaming experiences. You’ll take charge of leading development cycles, writing efficient code, and mentoring junior developers to deliver high-quality games. As a part of the team, you’ll contribute to creating our next major release, Silent Scream 2, and future projects.',
       responsibilities: ['Lead the development of complex game systems from concept to release', 'Collaborate with cross-functional teams to achieve seamless gameplay integration', 'Optimize performance and memory usage across platforms (PC/Console)',
         'Mentor and provide technical guidance to junior team members','Write clean, maintainable, and efficient code',
@@ -109,6 +131,8 @@ const CareersPageOpeningSection = () => {
       qualifications: ['Lead the development of complex game systems from concept to release', 'Collaborate with cross-functional teams to achieve seamless gameplay integration', 'Optimize performance and memory usage across platforms (PC/Console)',
         'Mentor and provide technical guidance to junior team members','Write clean, maintainable, and efficient code',
       ],
+      salary: 'BDT 10,000 / Month',
+      deadline: '31 November 2024',
       applyLink: 'https://form.jotform.com/242655786742065',
     },
   ];
@@ -132,10 +156,10 @@ const CareersPageOpeningSection = () => {
                 <JobRow
                   key={index}
                   title={job.title}
-                  work="On Site"
+                  status={job.status}
+                  workplace={job.workplace}
                   location={job.location}
-                  category="Game Development"
-                  type="Full Time"
+                  deadline={job.deadline}
                   onSeeDetails={() => setActiveModal(index)} // Open modal for the clicked job
                 />
               ))}
